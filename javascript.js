2e63c023a773c98deb9f8a61fdafbf475d94dc9f
ophanim777 <rikyjo49@gmail.com>
2026-02-22 14:49:24 +0700

    creating hash key

--- javascript.js
+++ javascript.js
@@ -4,4 +4,15 @@ class HashMap {
     this.capacity = capacity;
     this.buckets = new Array(this.capacity).fill(null).map(() => []);
     this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
   }}
